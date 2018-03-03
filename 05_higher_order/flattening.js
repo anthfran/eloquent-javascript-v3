2a2fb61b52cd71339e88db2a875c57c05a830e46
Anthony Francella <anthonyf@gmail.com>
2018-03-03 19:53:54 +0100

    completed flattening exercise

---
+++ 05_higher_order/flattening.js
@@ -0,0 +1,12 @@
+/*
+Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the original arrays.
+*/
+
+function flatten (arrays) {
+  return arrays.reduce((a,b) => a.concat(b))
+}
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+console.log(flatten(arrays))
+// → [1, 2, 3, 4, 5, 6]
