61eb4c4fb06d34e9adb41aa88c1f74e6171d04dc
Anthony Francella <anthonyf@gmail.com>
2018-03-05 09:41:33 +0100

    Updated fizzbuzz to correctly output string or number

--- 02_program_structure/fizzbuzz.js
+++ 02_program_structure/fizzbuzz.js
@@ -11,13 +11,5 @@ for (let n = 1; n < 101; n++) {
   let output = '';
   if (n % 3 == 0) output += "Fizz";
   if (n % 5 == 0) output += "Buzz";
-  console.log(n, output);
-}
-
-// only print numbers with FizzBuzz
-for (let n = 1; n < 101; n++) {
-  let output = '';
-  if (n % 3 == 0) output += "Fizz";
-  if (n % 5 == 0) output += "Buzz";
-  if (output != '') console.log(n, output);
+  console.log(output || n);
 }
